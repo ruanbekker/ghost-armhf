ed15e9393de1ebbf2155e06cb22bfa7652a890a8
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-09-04 00:43:53 +0200

    new script with env vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# docs
-# https://hub.docker.com/_/ghost/
-
-# allow the container to be started with `--user`
 if [[ "$*" == node*current/index.js* ]] && [ "$(id -u)" = '0' ];
   then
     chown -R node "$GHOST_CONTENT"
@@ -17,18 +13,129 @@ if [[ "$*" == node*current/index.js* ]];
     for src in "$baseDir"/*/ "$baseDir"/themes/*;
       do
         src="${src%/}"
-	target="$GHOST_CONTENT/${src#$baseDir/}"
-	mkdir -p "$(dirname "$target")"
-	if [ ! -e "$target" ];
+        target="$GHOST_CONTENT/${src#$baseDir/}"
+        mkdir -p "$(dirname "$target")"
+        if [ ! -e "$target" ];
           then
-	    tar -cC "$(dirname "$src")" "$(basename "$src")" | tar -xC "$(dirname "$target")"
-	fi
+            tar -cC "$(dirname "$src")" "$(basename "$src")" | tar -xC "$(dirname "$target")"
+        fi
       done
 
     knex-migrator-migrate --init --mgpath "$GHOST_INSTALL/current"
 fi
 
 # environment variables
-sed -i "s/localhost:2368/${SERVER_URL-localhost:2368}/"g /var/lib/ghost/config.production.json
+#sed -i "s/localhost:2368/${SERVER_URL-localhost:2368}/"g /var/lib/ghost/config.production.json
+prod() {
+cat > /var/lib/ghost/config.development.json << EOF
+{
+  "url": "http://${SERVER_URL:-localhost}:${SERVER_PORT:-2368}",
+  "server": {
+    "port": ${SERVER_PORT:-2368},
+    "host": "0.0.0.0"
+  },
+  "database": {
+    "client": "sqlite3",
+    "connection": {
+      "filename": "/var/lib/ghost/content/data/ghost.db"
+    }
+  },
+  "mail": {
+    "transport": "SMTP",
+    "from": "${FROM_NAME:-MyBlog} <${FROM_EMAIL:-ghost-blog@localhost}>",
+    "options": {
+      "service": "Mailgun",
+      "host": "${SMTP_HOST:-localhost}",
+      "port": ${SMTP_PORT:-25},
+      "auth": {
+        "user": "${SMTP_AUTH_USERNAME:-root}",
+        "pass": "${SMTP_AUTH_PASSWORD:-password}"
+      }
+    }
+  },
+  "logging": {
+    "transports": [
+      "file",
+      "stdout"
+    ]
+  },
+  "process": "systemd",
+  "paths": {
+    "contentPath": "/var/lib/ghost/content"
+  }
+}
+EOF
+}
+
+dev() {
+cat > /var/lib/ghost/config.development.json << EOF
+{
+  "url": "http://${SERVER_URL:-localhost}:${SERVER_PORT:-2368}",
+  "server": {
+    "port": ${SERVER_PORT:-2368},
+    "host": "0.0.0.0"
+  },
+  "database": {
+    "client": "sqlite3",
+    "connection": {
+      "filename": "/var/lib/ghost/content/data/ghost.db"
+    }
+  },
+  "mail": {
+    "transport": "Direct"
+  },
+  "logging": {
+    "transports": [
+      "file",
+      "stdout"
+    ]
+  },
+  "process": "systemd",
+  "paths": {
+    "contentPath": "/var/lib/ghost/content"
+  }
+}
+EOF
+}
+
+test(){
+cat > /var/lib/ghost/config.development.json << EOF
+{
+  "url": "http://localhost:2368",
+  "server": {
+    "port": 2368,
+    "host": "0.0.0.0"
+  },
+  "database": {
+    "client": "sqlite3",
+    "connection": {
+      "filename": "/var/lib/ghost/content/data/ghost.db"
+    }
+  },
+  "mail": {
+    "transport": "Direct"
+  },
+  "logging": {
+    "transports": [
+      "file",
+      "stdout"
+    ]
+  },
+  "process": "systemd",
+  "paths": {
+    "contentPath": "/var/lib/ghost/content"
+  }
+}
+EOF
+}
+
+if  [ "${ENV_TYPE}" = "PROD" ]
+  then prod
+
+elif [ "${ENV_TYPE}" = "DEV" ]
+  then dev
+  else test
+
+fi
 
 exec "$@"
